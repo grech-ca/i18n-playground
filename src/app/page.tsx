94be8deca01fae4983b839c46778fd097ccc162a
Mikhail Grechka <grech.code@gmail.com>
2024-02-20 12:04:44 +0100

    Update "Invalid template" message styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,7 +141,7 @@ const HomePage = () => {
         >
           <div className="font-medium flex divide-x-2 divide-gray-300 bg-white rounded-xl p-3 w-[36rem]">
             <div className="pr-2">Result:</div>
-            <div className="pl-2">{isTemplateValid ? interpolatedText : <Transition show appear enter="transition-all" enterFrom="-translate-x-2 opacity-0" enterTo="translate-x-0 opacity-100" className="py-1 px-2 rounded bg-red-100 text-red-600 font-medium">Invalid template</Transition>}</div>
+            <div className="pl-2">{isTemplateValid ? interpolatedText : <Transition show appear enter="transition-all" enterFrom="-translate-x-2 opacity-0" enterTo="translate-x-0 opacity-100" className="px-1 mx-1 ring-4 rounded ring-red-100 bg-red-100 text-red-600 font-medium">Invalid template</Transition>}</div>
           </div>
         </Transition>
       </div>
